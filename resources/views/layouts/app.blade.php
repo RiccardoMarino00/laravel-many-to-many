62dae94141f1b558519334ddc6be15a84bc90c4d
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-06-10 13:12:52 +0200

    improve: index style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class=" header-logged collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,36 +4,37 @@
 
 <section>
 
-    <div class="container">
+    {{-- <div class="container">
         <a href="{{route('admin.projects.create')}}">Create new project</a>
-    </div>
+    </div> --}}
 
     @foreach ($projects as $project)
-        <div class="m-2 border p-2 d-flex text-center">
-            <div class="col-6">
-                <p>{{$project->title}}</p>
-                <p>{{$project->slug}}</p>
-                <p>{{$project->content}}</p>
+        <div class="m-5  p-2 d-flex text-center">
+            <div class="col-6  p-3">
+                <p><strong>Title: </strong>  {{$project->title}}</p>
+                {{-- <p>{{$project->slug}}</p> --}}
+                <p> <strong> Content: </strong> {{$project->content}}</p>
                 {{-- @dump($project->type) --}}
-                <p>{{$project->type ? $project->type->name : 'Nessun tipo'}}</p>
+                <p> <strong>Type: </strong> {{$project->type ? $project->type->name : ' No Type'}}</p>
                 @foreach ($project->technologies as $technology)
                     {{-- <p>{{$project->technology ? $project->$technology->name : 'Nessuna tecnologia'}}</p> --}}
-                    <p>{{$technology->name}}</p>
+                    <p> <strong> Technology: </strong> {{$technology->name}}</p>
                 @endforeach
                 {{-- @dump($project->technologies) --}}
-                <a href="{{$project->link}}">{{$project->link}}</a>
-            </div>
-          
-            {{-- <div  role="group" aria-label="Default button group">
-              
-            </div> --}}
-            <div class="col-6 right-align">
+                <a href="{{$project->link}}"><strong> Link: </strong> {{$project->link}}</a> <br>
                 <a class="link-warning" href="{{route('admin.projects.edit', $project)}}">edit</a>
                 <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger">Delete</button>
                 </form>
+            </div>
+          
+            {{-- <div  role="group" aria-label="Default button group">
+              
+            </div> --}}
+            <div class="col-6 right-align p-3">
+                
             </div>
         </div>
     @endforeach
